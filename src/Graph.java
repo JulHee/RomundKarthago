d3de6fec55ea8354b2bad61c65e9dcddc09b1651
Namec <bruene@mathematik.uni-marburg.de>
2014-05-05 14:49:01 +0200

    Revert "Aufgabe 4 Rek Nullpointer BugFix"
    This reverts commit e8e75817991a551a9970c16257d9719ae007f579.

--- src/Graph.java
+++ src/Graph.java
@@ -97,7 +97,7 @@ public class Graph {
         HashSet<Knoten> retrn = new HashSet<Knoten>();
         String tmpSeiteStr = rekKnoten.seite.toString();
         HashSet<Knoten> tmpSet = getNachbarschaft(rekKnoten);
-        if(rekVorherigerKnoten!= null){tmpSet.remove(rekVorherigerKnoten);}
+        tmpSet.remove(rekVorherigerKnoten);
         for (Knoten i : tmpSet) {
             if (i.seite.toString() == tmpSeiteStr) {
                 retrn.add(i);
