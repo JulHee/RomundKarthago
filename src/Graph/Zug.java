3751735bf412e7bf34c9d4b88195b7845d49655d
peng0in <Julhee@gmx.de>
2014-05-13 12:26:03 +0200

    Zug Klasse erstellt mit Möglichkeit aus einem String zu erstellen und Funktionen Graph.convertToString

--- src/Graph/Zug.java
+++ src/Graph/Zug.java
@@ -15,11 +15,50 @@ public class Zug {
         this.stadt = stadt;
     }
 
+    public Zug(String zug){
+        String[] temp = zug.split(" ");
+        if (temp.length > 1){
+            try{
+                this.seite = readSeite(temp[0]);
+                this.stadt = Integer.parseInt(temp[1]);
+            } catch (Exception ex){
+                System.out.println(ex.getMessage());
+            }
+
+        }
+    }
+
+    private Seite readSeite(String x) throws Exception {
+        Seite resu;
+        if (x.equals("N")) {
+            resu = Seite.Neutral;
+        } else {
+            if (x.equals("R")) {
+                resu = Seite.Rom;
+            } else {
+                if (x.equals("C")) {
+                    resu = Seite.Kathargo;
+                } else {
+                    throw new Exception("Keine geeigneten Besetzer gefunden");
+                }
+            }
+        }
+        return resu;
+    }
+
     public Seite getSeite() {
         return seite;
     }
 
     public int getStadt() {
         return stadt;
+    }
+
+    @Override
+    public String toString() {
+        return "Zug{" +
+                "seite=" + seite +
+                ", stadt=" + stadt +
+                '}';
     }
 }
--- src/Main/Run.java
+++ src/Main/Run.java
@@ -13,13 +13,20 @@ public class Run {
     public static void main(String[] args) throws Exception{
 
         Graph graph = new Graph();
+        /*
         graph.read();
         graph.ausgeben();
+        Zug zug = new Zug("C 1");
+        Zug zug_org = new Zug(Seite.Kathargo,2);
+        System.out.println(zug.toString());
+        System.out.println(zug_org.toString());
 
         System.out.println(graph.getBesetztesGebiet(graph.findKnoten(6)));
         System.out.println(graph.getBesetztesGebiet(graph.findKnoten(9)));
 
 	    System.out.println("Punkte fuer Rom:      " + graph.besetztePunkteStandFuer(Seite.Rom));
 	    System.out.println("Punkte fuer Karthago: " + graph.besetztePunkteStandFuer(Seite.Kathargo));
+        System.out.println(graph.convertToString());
+        */
     }
 }
