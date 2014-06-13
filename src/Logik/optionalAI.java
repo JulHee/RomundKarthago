4e46be8b7a91d16e35d3f276803e6173a248564a
Joern Kabuth <joern.kabuth@gmx.net>
2014-06-13 16:34:23 +0200

    (optionale KI)beginn Kettenfkt etc.

--- src/Logik/optionalAI.java
+++ src/Logik/optionalAI.java
@@ -1,5 +1,8 @@
 package Logik;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 import Graph.*;
 
 /**
@@ -23,8 +26,8 @@ public class optionalAI extends AIPlayer{
 		return null;
 	}
 	/*
-	 * Konzept: Alle möglichen Züge werden bewertet und bekommen einen "Wert" zwischen 0 und 1 
-	 * zugewiesen, wobei der höhere Wert größere Erfolgschancen verspricht. 
+	 * Konzept: Alle möglichen Züge werden bewertet und bekommen einen "Wert" (evtl. zwischen 0 und 1) 
+	 * zugewiesen, wobei der höhere Wert größere Erfolgs/Gewinnchancen verspricht. 
 	 * 
 	 * Berechnung des Wertes durch:
 	 * 
@@ -39,7 +42,13 @@ public class optionalAI extends AIPlayer{
 	 * 
 	 * 
 	 */
-
+	/*
+	 * Variablen für Zugriffsrechte außerhalb definiert
+	 */
+	Graph myGraph = mechanik.getMyGraph();
+	ArrayList<ArrayList<Knoten>> Rchainz;
+	ArrayList<ArrayList<Knoten>> Kchainz;
+	
 
 	/**
 	 * Berechnung, wie "gut" ein Zug zu bewerten ist.
@@ -52,12 +61,48 @@ public class optionalAI extends AIPlayer{
 	// nicht fertig!
 	/**
 	 * Bestimmung der Ketten zusammenhängender Städte einer Fraktion
-	 * 
+	 * jeweils als ArrayList von ArrayList von Knoten ausgegeben
+	 *  
 	 */
 	public void getchainz(){
-		Graph myGraph = mechanik.getMyGraph();
+		ArrayList<ArrayList<Knoten>> Rchainz = new ArrayList<ArrayList<Knoten>>();
+		ArrayList<ArrayList<Knoten>> Kchainz = new ArrayList<ArrayList<Knoten>>();
+		int zaehl = 0; //sinnfreie zählvariable -> Anzahl der neutralen Knoten (als Test)
 		for(Knoten a: myGraph.l_knoten){
-			myGraph.getNachbarschaft(a);
-		}
-	}
+			if (a.getSeite() == Seite.Neutral){
+				zaehl+=1;
+			}else{
+				ArrayList<Knoten> chain = new ArrayList<Knoten>();
+				Seite site = a.getSeite();
+				kette(chain,site,a);
+				if(site==Seite.Rom){
+					Rchainz.add(chain);
+				}else{
+					Kchainz.add(chain);
+				}
+			}
+		};
+	};
+	/**
+	 * HilfsFunktion zur Erstellen der einzelnen Ketten als 
+	 * ArrayList<Knoten>
+	 * 
+	 * @param temp Kette, welche es ggf. zu verlängern gilt
+	 * @param seite	Fraktion der derzeitigen Kette
+	 * @param knot	zu betracchtendes Kettenglied
+	 * @return ArrayList<Knoten>
+	 */
+	//TODO Testen ob Knoten bereits betrachtet (letzter Knoten -> in Nachbarschaft von jetzigem)
+	public void kette(ArrayList<Knoten> temp,Seite seite,Knoten knot){
+		if(temp.contains(knot)) 
+		temp.add(knot);
+		HashSet<Knoten> blubb = myGraph.getNachbarschaft(knot);
+		for(Knoten x: blubb){
+			if(x.getSeite()== seite){
+				kette(temp,seite,x);
+			}
+		};
+		
+	};
+	
 }
