7918ea25de4ff1f134fbe713beff7551734e49ec
Namec <bruene@mathematik.uni-marburg.de>
2014-06-23 21:32:08 +0200

    network: Client weiter gearbeitet

--- src/network/Client.java
+++ src/network/Client.java
@@ -1,6 +1,6 @@
 package network;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 /**
@@ -9,10 +9,28 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args){
         try{
-            Socket clientR = new Socket(args[0],Integer.parseInt(args[1]));
+            Socket clientR = new Socket(args[0],Integer.parseInt(args[1]));        // Socket(Server-name,Port)
+            sendGameBoard (clientR);
+            while(true){
+                sendZug();
+            }
             //TODO Was der Client machen soll
 
         }catch (IOException e){e.getStackTrace();}
     }
 
+
+
+private void sendGameBoard(Socket clientR){
+    try {
+        BufferedReader in = new BufferedReader(new FileReader("ex/map.txt"));
+
+        OutputStream outS = clientR.getOutputStream();
+        BufferedOutputStream out = new BufferedOutputStream(outS);
+
+    }catch (IOException e){e.getStackTrace();}
+}
+private void sendZug(){
+    // TODO ZUG senden
+}
 }
