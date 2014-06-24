26f7d47de1368f1e614c9585be859e6a553f1cca
peng0in <Julhee@gmx.de>
2014-06-24 17:05:56 +0200

    ^(^^)

--- src/test/Run.java
+++ src/test/Run.java
@@ -3,9 +3,13 @@ package test;
 import logik.Mechanik;
 
 /**
- * Projekt: Rom und Karthago Author : Julian Heeger, Markus Poell, Christian
- * Bruene, Joern Kabuth Date : 26.04.14 Year : 2014
+ * Projekt: Rom und Karthago 
+ * Author : Julian Heeger, Markus Poell, Christian
+ * Bruene, Joern Kabuth
+ * Date : 26.04.14
+ * Year : 2014
  */
+
 public class Run {
 
 	public static void main(String[] args) throws Exception {
