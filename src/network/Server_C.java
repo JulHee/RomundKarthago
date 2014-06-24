f8428cc8af0923c9d8c6ba29a99385eab7734ace
Joern Kabuth <joern.kabuth@gmx.net>
2014-06-24 14:55:53 +0200

    Servaaaaaaaaa initiated

--- src/network/Server_C.java
+++ src/network/Server_C.java
@@ -1,5 +1,12 @@
 package network;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import core.datacontainers.Seite;
 import logik.Mechanik;
 
 /**
@@ -9,11 +16,22 @@ import logik.Mechanik;
  * Year    : 2014
  */
 public class Server_C {
-    Integer port;
-    Mechanik mechanik;
+    
+	  Seite mySeite = Seite.Kathargo;
+	  static Integer port;
+	  static Mechanik myMechanik;
 
     public Server_C(Integer port, Mechanik mechanik) {
         this.port = port;
-        this.mechanik = mechanik;
+        this.myMechanik = mechanik;
     }
+    public static void main(String[] args) {
+		try (ServerSocket ss = new ServerSocket(port)){
+			Socket clients = ss.accept();
+			DataOutputStream out = new DataOutputStream(clients.getOutputStream());
+            DataInputStream in = new DataInputStream(clients.getInputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
