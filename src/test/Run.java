8a3b92abc3f5ef2015b45d07b699d595656eff79
peng0in <Julhee@gmx.de>
2014-06-24 16:58:02 +0200

    Änderungen der Aufrufe passend zur neuen game() Implementierung

--- src/test/Run.java
+++ src/test/Run.java
@@ -2,22 +2,17 @@ package test;
 
 import logik.Mechanik;
 
-
-
-
 /**
- * Projekt: Rom und Karthago
- * Author : Julian Heeger, Markus Poell, Christian Bruene, Joern Kabuth
- * Date : 26.04.14
- * Year : 2014
+ * Projekt: Rom und Karthago Author : Julian Heeger, Markus Poell, Christian
+ * Bruene, Joern Kabuth Date : 26.04.14 Year : 2014
  */
 public class Run {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 
-     //  AutomatischeTest game = new AutomatischeTest();
-      Mechanik game = new Mechanik();
-      game.terminalGame();
+		// AutomatischeTest game = new AutomatischeTest();
+		Mechanik game = new Mechanik();
+		game.game();
 
 	}
 }
