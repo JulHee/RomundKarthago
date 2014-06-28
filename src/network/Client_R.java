958e01abe6dfa6242039136dff8e261d7647ad63
Joern Kabuth <joern.kabuth@gmx.net>
2014-06-28 11:11:07 +0200

    at work......network stuff

--- src/network/Client_R.java
+++ src/network/Client_R.java
@@ -21,104 +21,104 @@ import core.datacontainers.Zug;
  */
 public class Client_R {
 
-    Seite mySeite = Seite.Rom;
-    private Integer port = 0;
-    private String ip = "";
-    private final Mechanik myMechanik;
+	Seite mySeite = Seite.Rom;
+	private Integer port = 0;
+	private String ip = "";
+	private final Mechanik myMechanik;
 
-    public Client_R(Integer port, String ip, Mechanik m) {
-	this.port = port;
-	this.ip = ip;
-	this.myMechanik = m;
-    }
-
-    public Integer getPort() {
-	return port;
-    }
-
-    public String getIp() {
-	return ip;
-    }
-
-    public void aigegner(AIPlayer ai) {
-	try (Socket s = new Socket(ip, port)) {
-	    handleSocket_ai(s, ai);
-	} catch (IOException e) {
-	    e.getStackTrace();
-	}
-    }
-
-    public void humanEnemy() {
-	try (Socket s = new Socket(ip, port)) {
-	    handleSocket_hum(s);
-	} catch (IOException e) {
-	    e.getStackTrace();
-	}
-    }
-
-    public void handleSocket_ai(Socket s, AIPlayer ai) {
-	String input;
-	try {
-	    DataOutputStream out = new DataOutputStream(s.getOutputStream());
-	    DataInputStream in = new DataInputStream(s.getInputStream());
-
-	    // Senden der Map
-
-	    ArrayList<String> maptext = myMechanik.getMyGraph().getMaptext();
-	    for (String l : maptext) {
-		out.writeUTF(l);
-	    }
-	    while (myMechanik.getSpiel()) {
-
-		// Senden des Zuges
-		Zug zug = ai.nextZug();
-		out.writeUTF(zug.toFormat());
-
-		// Auswerten des Zuges
-		myMechanik.auswerten(zug.toFormat(), Seite.Rom);
-		in.available();
-		input = in.readUTF();
-		myMechanik.auswerten(input, Seite.Kathargo);
-	    }
-	    s.close();
-
-	} catch (IOException e) {
-	    e.printStackTrace();
+	public Client_R(Integer port, String ip, Mechanik m) {
+		this.port = port;
+		this.ip = ip;
+		this.myMechanik = m;
 	}
 
-    }
+	public Integer getPort() {
+		return port;
+	}
 
-    public void handleSocket_hum(Socket s) {
-	String input;
-	try {
-	    DataOutputStream out = new DataOutputStream(s.getOutputStream());
-	    DataInputStream in = new DataInputStream(s.getInputStream());
+	public String getIp() {
+		return ip;
+	}
 
-	    // Senden der Map
-	    ArrayList<String> maptext = myMechanik.getMyGraph().getMaptext();
-	    for (String l : maptext) {
-		out.writeUTF(l);
-	    }
-	    while (myMechanik.getSpiel()) {
-		System.out.println("Bitte Zug angeben:");
-		try {
-		    BufferedReader bufferRead = new BufferedReader(
-			    new InputStreamReader(System.in));
-		    String zug = bufferRead.readLine();
-		    out.writeUTF(zug);
-		    String move = myMechanik.auswerten(zug, Seite.Rom);
-		    in.available();
-		    input = in.readUTF();
-		    myMechanik.auswerten(input, Seite.Kathargo);
+	public void aigegner(AIPlayer ai) {
+		try (Socket s = new Socket(ip, port)) {
+			handleSocket_ai(s, ai);
 		} catch (IOException e) {
-		    e.getStackTrace();
+			e.getStackTrace();
 		}
-	    }
-	    s.close();
-
-	} catch (IOException e) {
-	    e.printStackTrace();
 	}
 
-    }
+	public void humanEnemy() {
+		try (Socket s = new Socket(ip, port)) {
+			handleSocket_hum(s);
+		} catch (IOException e) {
+			e.getStackTrace();
+		}
+	}
+
+	public void handleSocket_ai(Socket s, AIPlayer ai) {
+		String input;
+		try {
+			DataOutputStream out = new DataOutputStream(s.getOutputStream());
+			DataInputStream in = new DataInputStream(s.getInputStream());
+
+			// Senden der Map
+
+			ArrayList<String> maptext = myMechanik.getMyGraph().getMaptext();
+			for (String l : maptext) {
+				out.writeUTF(l);
+			}
+			while (myMechanik.getSpiel()) {
+
+				// Senden des Zuges
+				Zug zug = ai.nextZug();
+				out.writeUTF(zug.toFormat());
+
+				// Auswerten des Zuges
+				myMechanik.auswerten(zug.toFormat(), Seite.Rom);
+				in.available();
+				input = in.readUTF();
+				myMechanik.auswerten(input, Seite.Kathargo);
+			}
+			s.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void handleSocket_hum(Socket s) {
+		String input;
+		try {
+			DataOutputStream out = new DataOutputStream(s.getOutputStream());
+			DataInputStream in = new DataInputStream(s.getInputStream());
+
+			// Senden der Map
+			ArrayList<String> maptext = myMechanik.getMyGraph().getMaptext();
+			for (String l : maptext) {
+				out.writeUTF(l);
+			}
+			while (myMechanik.getSpiel()) {
+				System.out.println("Bitte Zug angeben:");
+				try {
+					BufferedReader bufferRead = new BufferedReader(
+							new InputStreamReader(System.in));
+					String zug = bufferRead.readLine();
+					out.writeUTF(zug);
+					String move = myMechanik.auswerten(zug, Seite.Rom);
+					in.available();
+					input = in.readUTF();
+					myMechanik.auswerten(input, Seite.Kathargo);
+				} catch (IOException e) {
+					e.getStackTrace();
+				}
+			}
+			s.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
--- src/network/Server_C.java
+++ src/network/Server_C.java
@@ -1,10 +1,13 @@
 package network;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 import core.datacontainers.Seite;
 import logik.Mechanik;
@@ -16,22 +19,52 @@ import logik.Mechanik;
  * Year    : 2014
  */
 public class Server_C {
-    
-	  Seite mySeite = Seite.Kathargo;
-	  static Integer port;
-	  static Mechanik myMechanik;
 
-    public Server_C(Integer port, Mechanik mechanik) {
-        this.port = port;
-        this.myMechanik = mechanik;
-    }
-    public static void main(String[] args) {
+	static Seite mySeite = Seite.Kathargo;
+	static Integer port;
+	static Mechanik myMechanik;
+
+	public Server_C(Integer port, Mechanik mechanik) {
+		this.port = port;
+		this.myMechanik = mechanik;
+	}
+	/**
+	 * Funktion zum Starten des Servers(Karthago)
+	 * Empfangen der Map mit anschließender Ausführung
+	 * des Spieles untereinader
+	 * @throws Exception
+	 */
+	public static void runserver() throws Exception{
 		try (ServerSocket ss = new ServerSocket(port)){
 			Socket clients = ss.accept();
 			DataOutputStream out = new DataOutputStream(clients.getOutputStream());
-            DataInputStream in = new DataInputStream(clients.getInputStream());
+			DataInputStream in = new DataInputStream(clients.getInputStream());
+
+			//Einlesen der Map, welche vom Client gesendet wird
+			ArrayList<String> map = new ArrayList<String>();
+			String line;
+			while((line = in.readUTF()).length() > 0){
+				map.add(line);
+			}
+			//TODO Start des eigentlichen Spiels
+			/*
+			 * Mechanik in client und Server sinnlos oder?
+			 * Server sollte Mechanik haben/kontrollieren
+			 */
+			while(myMechanik.getSpiel()){
+			System.out.println("Bitte geben Sie ihren Zug ein:");
+			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+			String zug = input.readLine();
+			out.writeUTF(zug);
+			myMechanik.auswerten(zug, mySeite);
+			in.readUTF();
+			}
+			
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	public static void main(String[] args) throws Exception {
 	}
 }
