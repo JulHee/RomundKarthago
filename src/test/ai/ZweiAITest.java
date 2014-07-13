62099101270e8177b91ae628d6a7c0a9e6f49b2c
Namec <bruene@mathematik.uni-marburg.de>
2014-07-13 14:47:39 +0200

    test: 2 ai test

---
+++ src/test/ai/ZweiAITest.java
@@ -0,0 +1,22 @@
+package test.ai;
+
+import core.datacontainers.Seite;
+import logik.Mechanik;
+import logik.ai.Scrooge;
+import logik.ai.Sloth;
+import logik.ai.WaspAI;
+import org.junit.Test;
+
+/**
+ * Created by Acer on 13.07.2014.
+ */
+public class ZweiAITest {
+    @Test
+    public void testNextZug() throws Exception {
+        Mechanik myMechanik = new Mechanik("ext/aiTestMap.txt");
+        Sloth mySloth = new Sloth(Seite.Rom, myMechanik);
+        Scrooge myScrooge = new Scrooge(Seite.Kathargo, myMechanik);
+
+        myMechanik.game(mySloth, myScrooge);
+    }
+}
