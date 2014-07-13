fbb2ee8e73640cb65f1f00ecf4f5a364153eb450
Namec <bruene@mathematik.uni-marburg.de>
2014-07-13 15:56:07 +0200

    core: graph Debug

--- src/core/Graph.java
+++ src/core/Graph.java
@@ -475,7 +475,7 @@ public class Graph implements Cloneable {
                 } else {
                     // TODO Bitte verändern wurde nur zu Testzwecken mit println versehen
                     String tempGraphZustand = temp.ssuf(temp, myzug).convertToString();
-                    System.out.println("tempGraphZustand: " +tempGraphZustand);
+                    System.out.println("tempGraphZustand: " +tempGraphZustand+ " | Spieler "+ spieler + "| Zug "+ myzug);
                     if (history.contains(tempGraphZustand)) {
                         retrnZustand.setName(this.convertToString());
                         retrnZustand.setErrorcode(3);
